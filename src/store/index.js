5932dac759400ff413148a0d58ef94fc77a9d901
Monsieurmm <51748513+Monsieurmm@users.noreply.github.com>
2020-06-25 16:00:15 +0200

    chore: added cart state management

--- src/store/index.js
+++ src/store/index.js
@@ -6,6 +6,38 @@ import sliders from "./modules/sliders";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
+
+  state: {
+    cart: []
+  },
+
+  mutations: {
+    addToCart (state, payload) {
+      state.cart.push( Number(payload) )
+    },
+    decrementProductInventory(state, payload) {
+      let product = state.products.find(product => product.id === Number(payload))
+      product.quantity--;
+    }
+  },
+
+  actions: {
+    addToCart ({ commit }, payload) {
+      commit("addToCart", payload)
+      commit("decrementProductInventory", payload)
+    }
+  },
+
+  getters: {
+    cartItems: (state) => {
+      return state.cart.map(
+        productId => state.products.find(
+            product => product.id === productId
+        )
+      )
+    }
+  },
+
   modules: {
     products,
     sliders
