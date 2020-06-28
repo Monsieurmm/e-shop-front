3f78086ebeaa4dcda5560705ec94c5b5081cbe44
Monsieurmm <51748513+Monsieurmm@users.noreply.github.com>
2020-06-28 16:42:27 +0200

    chore: update app.vue with new snackbar

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,9 @@
     <LeftNavDrawer />
 
     <v-content class="mx-4 mb-4">
-      <Snackbar />
       <v-snackbar
         :text="text"
+        :color="color"
         :timeout="timeout"
         :top="y === 'top'"
         :bottom="y === 'bottom'"
@@ -16,37 +16,38 @@
         v-model="snackbar"
       >
         {{ text }}
-        <v-btn flat dark @click.native="snackbar = false">
-          Close
+        <v-btn text dark @click.native="snackbar = false">
+          <v-icon green text>close</v-icon>
         </v-btn>
       </v-snackbar>
+
       <router-view v-on:showSnackbar="showSnackbar"></router-view>
     </v-content>
   </v-app>
 </template>
 
 <script>
-import Snackbar from "./components/partials/Snackbar";
 import LeftNavDrawer from "./components/partials/LeftNavDrawer";
 export default {
   name: "App",
 
   components: {
-    LeftNavDrawer,
-    Snackbar
+    LeftNavDrawer
   },
 
   data: () => ({
     snackbar: false,
     text: "",
+    color: "",
     timeout: null,
     y: null,
     x: null,
     mode: null
   }),
   methods: {
-    showSnackbar(text, timeout = 3000, yPos = "top", xPos, mode) {
+    showSnackbar(text, color, timeout = 3000, yPos = "top", xPos, mode) {
       this.text = text;
+      this.color = color;
       this.timeout = timeout;
       this.y = yPos;
       this.x = xPos;
