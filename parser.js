4693793eb62e3222df0752fa46967de48f6be817
Brad Niepceron <87471823+bniepce@users.noreply.github.com>
2021-10-01 17:56:00 +0200

    Update parser.js

--- parser.js
+++ parser.js
@@ -3,32 +3,5 @@ var jsonDates = {
     parse: function(obj){
         var parsedObj = JSON.parse(obj);
         return this.parseDates(parsedObj);
-    },
-    parseDates: function(obj){
-      // iterate properties
-      for(pName in obj){
-        
-        // make sure the property is 'truthy'
-        if (obj[pName]){
-          var value = obj[pName];
-          // determine if the property is an array
-          if (Array.isArray(value)){
-            for(var ii = 0; ii < value.length; ii++){
-              this.parseDates(value[ii]);
-            }
-          }
-          // determine if the property is an object
-          else if (typeof(value) == "object"){
-            this.parseDates(value);
-          }
-          // determine if the property is a string containing a date
-          else if (typeof(value) == "string" && this.dtrx2.test(value)){
-            // parse and replace
-            obj[pName] = new Date(obj[pName]);
-          }
-        }
-      }
-  
-      return obj;
     }
   };
